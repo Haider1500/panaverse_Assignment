3ec3f25cf19403e2e8e0a24954d2168718f6b669
Ali Haider <ali1500haider@gmail.com>
2024-04-20 17:20:52 +0500

    till question14

---
+++ Question14/index.ts
@@ -0,0 +1,8 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+const guestList = ["Grand Father", "Mother", "Sister"];
+const message =
+  "I would be very glad, if you could join me for dinner on this coming weekend";
+console.log(`${guestList[0]}, ${message}`);
+console.log(`${guestList[1]}, ${message}`);
+console.log(`${guestList[2]}, ${message}`);
